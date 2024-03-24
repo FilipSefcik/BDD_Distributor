71f05b2edd6b1167f357cf9759bc7a1f2308cb3e
FilipSefcik <sefcik.filip02@gmail.com>
2024-03-24 16:30:44 +0100

    now added bash

---
+++ BDD_Distributor/BDD_Distributor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Should be used only in build directory!!!!!! This is here only because of github!!!!!
+
+# Prompt user for number of processes
+read -p "Enter the number of processes: " num_processes
+
+# Prompt user for path to config file
+read -p "Enter the path to the config file: " conf_path
+
+# Prompt user for number of dividers
+read -p "Enter the number of divider: " num_divider
+
+read -p "Enter which state you want availability to be calculated [0/1]: " state
+
+# Construct mpiexec command
+mpi_command="mpiexec -n $num_processes BDD_Distributor/main $conf_path $num_divider $state"
+
+# Print the constructed command
+echo "Executing command: $mpi_command"
+
+# Execute the mpiexec command
+$mpi_command
