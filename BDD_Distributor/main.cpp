07cb2e781f3021d401f810050a74f1407204fd4c
FilipSefcik <sefcik.filip02@gmail.com>
2024-03-31 00:03:17 +0100

    edited main

--- BDD_Distributor/main.cpp
+++ BDD_Distributor/main.cpp
@@ -1,11 +1,6 @@
 #include "BDD_Distributor.h"
 
 int main(int argc, char* argv[]) {
-
-    //std::string conf_path = "module_map.conf";
-    //int divider_flag = 0;
-    //int state = 1;
-
     std::string conf_path = argc > 1 ? argv[1] : "module_map.conf";
     int divider_flag = argc > 2 ? std::stoi(argv[2]) : 0;
     int state = argc > 3 ? std::stoi(argv[3]) : 1;
